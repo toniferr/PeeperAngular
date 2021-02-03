69feed1d5c23cfbdb0de78d0c70aee5661ced556
toni <ferreirocouto@gmail.com>
2021-02-03 21:15:37 +0100

    añadido otro componente

---
+++ src/app/zapatillas/zapatillas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'zapatillas',
+    templateUrl: './zapatillas.component.html'
+})
+export class ZapatillasComponent {
+
+    public titulo: string = "Componente de zapatillas";
+
+}

446bac0d8a34dcd4a95edbca637ec99a937da0e6
toni <ferreirocouto@gmail.com>
2021-02-05 12:30:02 +0100

    trabajando con directivas

--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -9,16 +9,32 @@ export class ZapatillasComponent implements OnInit{
 
     public titulo: string = "Componente de zapatillas";
     public zapatillas: Array<Zapatilla>;
+    public marcas: String[]; //otra forma de declarar array
+    public color: string;
+
     constructor(){
+        this.color = 'yellow';
+        this.marcas = new Array();
         this.zapatillas = [
-            new Zapatilla('Reebok Classic', 'Reebok', 'blanco', true, 80),
-            new Zapatilla('Nike Runner', 'Nike', 'rojo', true, 90),
-            new Zapatilla('Adidas yezzy', 'Adidas', 'negro', true, 85)        
+            new Zapatilla('Reebok Classic', 'Reebok', 'Blanco', true, 80),
+            new Zapatilla('Nike Runner', 'Nike', 'Rojo', true, 70),
+            new Zapatilla('Adidas yezzy', 'Adidas', 'Negro', true, 85),  
+            new Zapatilla('Nike otro', 'Nike', 'Amarillo', true, 60),
+            new Zapatilla('All Star Converse', 'Converse', 'Verde', false, 180)       
         ];
     }
 
     ngOnInit(){
         console.log(this.zapatillas);
+        this.getMarcas();
+    }
+
+    getMarcas(){
+        this.zapatillas.forEach((zapatilla, index) => {
+            if (this.marcas.indexOf(zapatilla.marca) < 0){
+                this.marcas.push(zapatilla.marca);
+            }
+        })
     }
 
 }

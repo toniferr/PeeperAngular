f4963ea9561d3232da38d1d3208b6bec2035fa83
toni <ferreirocouto@gmail.com>
2021-02-05 20:15:06 +0100

    else con ngtemplate

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public identificado: boolean;
+
+  constructor() { 
+    this.identificado = false;
+  }
 
   ngOnInit(): void {
+  }
+
+  setIdentificado(){
+    this.identificado = true;
+  }
+
+  
+  unsetIdentificado(){
+    this.identificado = false;
   }
 
 }
